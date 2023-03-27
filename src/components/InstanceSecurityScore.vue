dc774198cfa0c8c6654987edf1d1a59bcc8b11a8
Swati <swati11129@gmail.com>
2023-03-28 02:11:02 +0530

    commit 2

--- src/components/InstanceSecurityScore.vue
+++ src/components/InstanceSecurityScore.vue
@@ -1,68 +1,61 @@
 <template>
 <div style="background-color:#d1dde8; width:100vw; height:100vh">
-<v-container class=" white my-5">
-    <v-container class="text my-4 font-weight-bold text-h5 white  ">Instance Security Score</v-container>
+    <v-container class=" white my-5">
+        <v-container class="text my-4 font-weight-bold text-h5 white  ">Instance Security Score</v-container>
 
-    <v-list>
-        <v-list-item-group color="primary">
-            <v-list-item class="font-weight-bold" v-for="(title, i) in titles" :key="i">
-                <v-list-item-content>
-                    <v-list-item-title v-text="title.text"></v-list-item-title>
-                </v-list-item-content>
-                <v-list-item-content>
-                    <v-list-item-title v-text="title.score"> </v-list-item-title>
-                    
-                </v-list-item-content>
-                <v-list-item-content>
-                    
-                    <v-list-item-title v-text="title.priority"></v-list-item-title>
-                </v-list-item-content>
-            </v-list-item>
-        </v-list-item-group>
-    </v-list>
-
-    <v-list>
-        <v-list-item-group color="primary">
-            <v-list-item class="my-5" v-for="(item, i) in items" :key="i">
-                <v-list-item-content>
-                    <v-list-item-title v-text="item.text"></v-list-item-title>
-                </v-list-item-content>
-                <v-list-item-content class="mx-5">
-
-                    <span style="width:2px; background-color:purple; height:3px"></span>
-                    <v-list-item-title v-text="item.score"></v-list-item-title>
-                </v-list-item-content>
-               
+        <v-list>
+            <v-list-item-group color="primary">
+                <v-list-item class="font-weight-bold" v-for="(title, i) in titles" :key="i">
                     <v-list-item-content>
-
-                        <v-progress-linear style="width:100px" class="rounded-pill" v-model="item.high_priority_issue" height="10"  />
+                        <v-list-item-title v-text="title.text"></v-list-item-title>
                     </v-list-item-content>
-                    
+                    <v-list-item-content>
+                        <v-list-item-title v-text="title.score"> </v-list-item-title>
+
+                    </v-list-item-content>
                     <v-list-item-content>
 
+                        <v-list-item-title v-text="title.priority"></v-list-item-title>
+                    </v-list-item-content>
+                </v-list-item>
+            </v-list-item-group>
+        </v-list>
+
+        <v-list>
+            <v-list-item-group color="primary">
+                <v-list-item class="my-5" v-for="(item, i) in items" :key="i">
+                    
+                    <v-list-item-content>
+                        <v-list-item-title v-text="item.text"></v-list-item-title>
+                    </v-list-item-content>
+
+                    <v-list-item-content class="mx-5">
+                        <span style="width:2px; background-color:purple; height:3px"></span>
+                        <v-list-item-title v-text="item.score"></v-list-item-title>
+                    </v-list-item-content>
+
+                    <v-list-item-content>
+                        <v-progress-linear v-show="isLoading" class="rounded-pill" v-model="item.high_priority_issue" height="10" />
                         <v-list-item-title v-text="item.high_priority_issue"></v-list-item-title>
                     </v-list-item-content>
-                   
-              
-            </v-list-item>
-        </v-list-item-group>
-    </v-list>
+                </v-list-item>
+            </v-list-item-group>
+        </v-list>
 
-</v-container>
+    </v-container>
 </div>
 </template>
 
 <script>
 export default {
-    data: () => ({       
+    data: () => ({
+        isLoading: true,
         titles: [{
-                text: 'Instance Name',
-                score: 'Score',
-                priority: 'High Priority Issue'
-            },            
-        ],
-        items: [
-            {
+            text: 'Instance Name',
+            score: 'Score',
+            priority: 'High Priority Issue'
+        }, ],
+        items: [{
                 text: 'Instance_Dev',
                 score: 91,
                 high_priority_issue: 100
@@ -88,8 +81,14 @@ export default {
                 high_priority_issue: 20
             },
         ],
-       
+
     }),
+
+    watch() {
+        data: {
+            this.isLoading = false
+        }
+    },
 }
 </script>
 
@@ -102,5 +101,4 @@ export default {
 .text {
     background: #d24b4b;
 }
-
 </style>
