fa9b7687955f6541900d2b2d0a0d30e66c290c63
Swati <swati11129@gmail.com>
2023-03-29 16:55:56 +0530

    color changed

--- src/components/InstanceSecurityScore.vue
+++ src/components/InstanceSecurityScore.vue
@@ -1,17 +1,17 @@
 <template>
 <div style="background-color:#d1dde8; width:100vw; height:100vh">
-    <v-container class=" white my-5 cyan" style="width: 60%;">
-        <div class="text my-5 font-weight-bold text-h5 white  ">Instances Security Score</div>
+    <v-container class=" white my-5 " style="width: 60%;">
+        <div class="text my-5 font-weight-bold text-h5   ">Instances Security Score</div>
         <div>
-            <div class="font-weight-bold d-flex pink my-8" v-for="(title, i) in titles" :key="i">
-                <span style="width:35%" class="blue">
+            <div class="font-weight-bold d-flex  my-8" v-for="(title, i) in titles" :key="i">
+                <span style="width:35%" class="">
                     <div v-text="title.text"></div>
                 </span>
-                <div style="" class="white mx-auto d-flex">
+                <div style="" class=" mx-auto d-flex">
                     <img src="../assets/score icon.jpg" width="25" height="20" class="mr-2 my-auto">
                     <div v-text="title.score"> </div>
                 </div>
-                <span class="white mx-auto" style="width: 15%;">  
+                <span class=" mx-auto" style="width: 15%;">  
                     <div v-text="title.priority"></div>
                 </span>
             </div>
@@ -19,15 +19,15 @@
 
         <div>
             <div color="primary">
-                <div class="my-8 d-flex indigo" v-for="(item, i) in items" :key="i">
-                    <div class="blue my-auto" style="width:35%">
+                <div class="my-8 d-flex " v-for="(item, i) in items" :key="i">
+                    <div class=" my-auto" style="width:35%">
                         <div v-text="item.text"></div>
                     </div>
-                    <div  class="d-flex mx-auto white my-auto" style="">
+                    <div  class="d-flex mx-auto  my-auto" style="">
                         <div  style="width:25px; height:13px" :style="{'background-color':score_color(item.score)}" class="rounded-pill my-auto mr-2  "/>
                         <div v-text="item.score"/>
                     </div>
-                    <div class="white my-auto mx-auto d-flex justify-space-between" style="width:15%">
+                    <div class=" my-auto mx-auto d-flex justify-space-between" style="width:15%">
                             <v-progress-linear   class="rounded-pill yellow mr-2 my-auto" v-model="item.high_priority_issue" height="13"  />                        
                         <div v-text="item.high_priority_issue" />
                     </div>
